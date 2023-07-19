afa4dec619113f1d0d5823655852617eb51af937
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-19 14:15:26 +0200

    dev: added getting json file

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
-var chartDom = document.querySelector("div.diagram");
-if (chartDom === null) {
-  throw new Error("div.diagram not found");
-}
-var myChart = echarts.init(chartDom, "vintage");
-var option = {
-  xAxis: {
-    type: "category",
-    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-  },
-  yAxis: {
-    type: "value",
-  },
-  series: [
-    {
-      data: [120, 200, 150, 80, 70, 110, 130],
-      type: "bar",
-      showBackground: true,
-      backgroundStyle: {
-        color: "rgba(180, 180, 180, 0.2)",
-      },
-    },
-  ],
-};
+(async function () {
+  // recupere le repository.json
+  const response = await fetch("./repositories.json");
+  const json = await response.json();
+  console.log("json: ", json);
 
-myChart.setOption(option);
+  var chartDom = document.querySelector("div.diagram");
+  if (chartDom === null) {
+    throw new Error("div.diagram not found");
+  }
+  var myChart = echarts.init(chartDom, "vintage");
+  var option = {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: "bar",
+        showBackground: true,
+        backgroundStyle: {
+          color: "rgba(180, 180, 180, 0.2)",
+        },
+      },
+    ],
+  };
+
+  myChart.setOption(option);
+})();
