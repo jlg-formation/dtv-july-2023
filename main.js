a7a8854c9dddb1ce1fb12f5fe87c580c08c8365f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-19 09:45:33 +0200

    dev: added type module
    - added type="module"
    - inserted example of echarts code
    - handle null pointer for chartDom

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 import * as echarts from "echarts";
 
-console.log("Start the bar diagram");
+var chartDom = document.querySelector("div.diagram");
+if (chartDom === null) {
+  throw new Error("div.diagram not found");
+}
+var myChart = echarts.init(chartDom);
+var option = {
+  xAxis: {
+    type: "category",
+    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: "bar",
+      showBackground: true,
+      backgroundStyle: {
+        color: "rgba(180, 180, 180, 0.2)",
+      },
+    },
+  ],
+};
 
-var a = 34.12;
-console.log("a: ", a);
+myChart.setOption(option);
