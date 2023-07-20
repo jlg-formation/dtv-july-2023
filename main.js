00e46b1514710202c6831ac4c6d9117549b1eea0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-20 14:14:13 +0200

    dev: scatter

--- main.js
+++ main.js
@@ -1,83 +1,26 @@
 (async function () {
-  // recupere le repository.json
-  const response = await fetch(
-    "https://api.github.com/search/repositories?q=stars%3A%3E10000+data-visualization&sort=stars&order=desc"
-  );
-  const json = await response.json();
-
-  const repositories = json.items;
-
-  const filterRepos = repositories.filter(
-    (repo) =>
-      ![
-        "GitHub-Chinese-Top-Charts",
-        "helm",
-        "data-science",
-        "umami",
-        "tui.editor",
-        "charts",
-        "analytics",
-        "sampler",
-        "netdata",
-        "pixijs",
-        "metabase",
-        "directus",
-        "awesome-datascience",
-        "Data-Science-For-Beginners",
-        "awesome-creative-coding",
-        "best-of-ml-python",
-        "awesome-bigdata",
-      ].includes(repo.name)
-  );
-
-  const repos = filterRepos.map((repository) => ({
-    name: repository.full_name,
-    stars: repository.stargazers_count,
-  }));
-  console.log("repos: ", repos);
-
+  const generateData = () => {
+    const array = [];
+    for (let i = 0; i < 10000; i++) {
+      const item = [Math.random(), Math.random()];
+      array.push(item);
+    }
+    return array;
+  };
+  const data = generateData();
   var chartDom = document.querySelector("div.diagram");
   if (chartDom === null) {
     throw new Error("div.diagram not found");
   }
   var myChart = echarts.init(chartDom, "vintage");
   var option = {
-    grid: {
-      top: 10,
-      bottom: 50,
-      left: 200,
-      right: 30,
-    },
-    axisLabel: {
-      color: "black",
-    },
-    xAxis: {
-      type: "value",
-    },
-    yAxis: {
-      type: "category",
-      data: repos.map((r) => r.name).reverse(),
-      axisLabel: {
-        show: true,
-        fontSize: 15,
-      },
-    },
+    xAxis: {},
+    yAxis: {},
     series: [
       {
-        data: repos.map((r) => r.stars).reverse(),
-        type: "bar",
-        showBackground: true,
-        backgroundStyle: {
-          color: "rgba(180, 180, 180, 0.2)",
-        },
-        label: {
-          show: true,
-          precision: 1,
-          position: "right",
-          fontFamily: "monospace",
-          color: "black",
-          fontWeight: "bold",
-        },
+        symbolSize: 5,
+        data: data,
+        type: "scatter",
       },
     ],
   };
