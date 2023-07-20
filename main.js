c2a02500b402b8e3ee1a19f044e8dc3a8eab8a2c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-20 15:48:05 +0200

    dev: geojson example

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
 (async function () {
-  const generateData = () => {
-    const array = [];
-    for (let i = 0; i < 10000; i++) {
-      const item = [Math.random(), Math.random()];
-      array.push(item);
-    }
-    return array;
-  };
-  const data = generateData();
   var chartDom = document.querySelector("div.diagram");
   if (chartDom === null) {
     throw new Error("div.diagram not found");
   }
-  var myChart = echarts.init(chartDom, "vintage");
+  var myChart = echarts.init(chartDom);
+
+  myChart.showLoading();
+  const response = await fetch("./chezmoi.json");
+  const json = await response.json();
+
+  myChart.hideLoading();
+  echarts.registerMap("chezmoi", json);
   var option = {
-    xAxis: {},
-    yAxis: {},
+    title: {
+      text: "USA Population Estimates (2012)",
+      subtext: "Data from www.census.gov",
+      sublink: "http://www.census.gov/popest/data/datasets.html",
+      left: "right",
+    },
+    tooltip: {
+      trigger: "item",
+      showDelay: 0,
+      transitionDuration: 0.2,
+    },
     series: [
       {
-        symbolSize: 5,
-        data: data,
-        type: "scatter",
+        name: "USA PopEstimates",
+        type: "map",
+        roam: true,
+        map: "chezmoi",
+        emphasis: {
+          label: {
+            show: true,
+          },
+        },
+        data: [],
       },
     ],
   };
-
   myChart.setOption(option);
 })();
